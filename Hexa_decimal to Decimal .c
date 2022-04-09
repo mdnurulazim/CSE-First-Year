939e5fff7f9be44c4fcf1aad056d1232ae5213c5
Md Nurul Azim <103141055+mdnurulazim@users.noreply.github.com>
2022-04-09 13:59:51 +0600

    Add files via upload
    My Practice

---
+++ Hexa_decimal to Decimal .c
@@ -0,0 +1,13 @@
+//Hexa-Decimal to Decimal
+#include <stdio.h>
+int main ()
+{
+int number;
+printf("Enter Hexa-Decimal Number :");
+scanf("%x",& number);
+printf("Decimal Number :%d", number );
+
+return 0 ;
+
+
+}
